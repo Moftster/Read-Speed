86ab027b0baa13881027383e794e06678a072c0b
David Moftakhar <david@sellmysnaps.com>
2017-08-16 09:56:42 +0100

    got jquery working

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-/*alert("JQuery is working!");*/ 
-})
+/* alert("JQuery is working!"); */
+
+$("button").addClass("animated bounce");
+
+$(".well").addClass("animated shake");
+
+$("#target3").addClass("animated fadeOut");
+
+});
