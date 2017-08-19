3c245d17e6d924784609e40facac0dc0a283f768
David Moftakhar <david@sellmysnaps.com>
2017-08-19 14:53:32 +0100

    latest updates including messign with jquery

--- script/script.js
+++ script/script.js
@@ -7,4 +7,26 @@ $(".well").addClass("animated shake");
 
 $("#target3").addClass("animated fadeOut");
 
+$("#target5").removeClass("animated bounce");
+
+$("h1").css("color", "red");
+
+$("#target4").prop("disable", true);
+
+
+$("#headingid").remove();
+
+$("#target2").appendTo("#right-well");
+
+$("#fadeinout").fadeIn(2500);
+
+$("#target4").clone().appendTo("#left-well");
+
+$("#target4").parent().css("background-color", "#FFFFE0");
+
+$("#right-well").children().css("color", "orange");
+
+$(".target:nth-child(1)").addClass("animated shake");
+
+
 });
